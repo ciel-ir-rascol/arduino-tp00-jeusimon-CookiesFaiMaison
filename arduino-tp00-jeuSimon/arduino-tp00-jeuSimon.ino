6c83ff510802529f0d14f9fcae31bf9e14a83384
CookiesFaiMaison <112569301+CookiesFaiMaison@users.noreply.github.com>
2025-01-23 17:30:30 +0100

    pas fini 2

--- arduino-tp00-jeuSimon/arduino-tp00-jeuSimon.ino
+++ arduino-tp00-jeuSimon/arduino-tp00-jeuSimon.ino
@@ -7,6 +7,8 @@
 int etatBoutonR; 
 int etatBoutonB; 
 int tirage [4] {0,0,0,0};
+int joueur[4] {5,5,5,5};
+int compte = 0;
 
 bool start = true;
 
@@ -31,37 +33,62 @@ void randtirage(){
   }
 }
 
-
+void affichage(){
+  for (int i=0; i<4; i++){
+    if(tirage[i] == 0){
+      Serial.print("V");
+    } else {
+      Serial.print("R");
+    }
+  }
+}
 
 void loop()                        
 {
       etatBoutonR = digitalRead(BP_R);        // rappel : bouton = 3
       etatBoutonB = digitalRead(BP_B);        // rappel : bouton = 3
 
-  if (etatBoutonR == HIGH)              // test si le bouton a un niveau logique haut
-  {
-   digitalWrite(LED_R, HIGH);             // la led 1 s'allume*
-   delay(1000);
-  }
-  
-  else (etatBoutonR == LOW);           // si le bouton a un niveau logique différent de HIGH
-  {
-    digitalWrite(LED_R, LOW);             // la led 1 s'allume
-  }
+  if (compte < 4){
+    if (etatBoutonR == HIGH)              // test si le bouton a un niveau logique haut
+    {
+    digitalWrite(LED_R, HIGH);             // la led 1 s'allume*
+    delay(1000);
+    joueur[compte] = 1;
+    compte++;
+    }
+    
+    else (etatBoutonR == LOW);           // si le bouton a un niveau logique différent de HIGH
+    {
+      digitalWrite(LED_R, LOW);             // la led 1 s'allume
+    }
 
-  if (etatBoutonB == HIGH)              // test si le bouton a un niveau logique haut
-  {
-   digitalWrite(LED_B, HIGH);             // la led 1 s'allume
-   delay(1000);
-  }
-  
-  else (etatBoutonB == LOW);           // si le bouton a un niveau logique différent de HIGH
-  {
-    digitalWrite(LED_B, LOW);             // la led 1 s'allume
+    if (etatBoutonB == HIGH)              // test si le bouton a un niveau logique haut
+    {
+    digitalWrite(LED_B, HIGH);             // la led 1 s'allume
+    delay(1000);
+    joueur[compte] = 0;
+    compte++;
+
+    }
+    
+    else (etatBoutonB == LOW);           // si le bouton a un niveau logique différent de HIGH
+    {
+      digitalWrite(LED_B, LOW);             // la led 1 s'allume
+    }
   }
 
   if (start == true){
     start = false;
     randtirage();
+    affichage();
   }
-}
+
+  if (compte == 4){
+    if (joueur != tirage){
+      Serial.print("Faux");
+    } else {
+      Serial.print("Vrai");
+    }
+    start = true;
+  }
+  }
